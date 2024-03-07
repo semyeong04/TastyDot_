3912b3c2006866be7da98263cf44ba4986887e51
user <auddl1460@naver.com>
2024-03-07 16:25:10 +0900

    박세명 사업자화면 커밋

---
+++ app/src/main/java/com/project/project3/MenuRegiActivity.java
@@ -0,0 +1,14 @@
+package com.project.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuRegiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_regi);
+    }
+}
