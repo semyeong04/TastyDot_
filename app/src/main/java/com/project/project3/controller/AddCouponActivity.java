afd370f8c85070143386442f79d158a9fb5019bd
user <auddl1460@naver.com>
2024-03-08 14:24:33 +0900

    박세명 회원가입, 쿠폰발급 프론트 커밋

---
+++ app/src/main/java/com/project/project3/controller/AddCouponActivity.java
@@ -0,0 +1,16 @@
+package com.project.project3.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.project3.R;
+
+public class AddCouponActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_coupon);
+    }
+}
--- app/src/main/java/com/project/project3/view/RegiViewHolder.java
+++ app/src/main/java/com/project/project3/view/RegiViewHolder.java
@@ -1,4 +1,48 @@
 package com.project.project3.view;
 
-public class RegiViewHolder {
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.project.project3.R;
+
+public class RegiViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    private Button button;
+    private EditText etSize;
+    private  EditText etPrice;
+
+    ItemClickListener listener;
+
+   public RegiViewHolder(View price){
+       super(price);
+       // 변수 초기화
+       this.button = price.findViewById(R.id.btnDel);
+       this.etSize = price.findViewById(R.id.etSize);
+       this.etPrice = price.findViewById(R.id.etPrice);
+
+       // 버튼에 클릭이벤트 연결
+       this.button.setOnClickListener(this);
+
+   }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public EditText getEtSize() {
+        return etSize;
+    }
+
+    public EditText getEtPrice() {
+        return etPrice;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        this.listener.onItemClick(v,getAdapterPosition());
+    }
 }
