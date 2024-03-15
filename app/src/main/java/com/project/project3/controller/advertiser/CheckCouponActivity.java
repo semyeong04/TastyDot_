f6feff08d2298d470082dab73167cd876daa7bda
user <auddl1460@naver.com>
2024-03-15 09:53:13 +0900

    박세명 광고주 네비게이션바 삭제 커밋

--- app/src/main/java/com/project/project3/controller/advertiser/CheckCouponActivity.java
+++ app/src/main/java/com/project/project3/controller/advertiser/CheckCouponActivity.java
@@ -27,38 +27,6 @@ public class CheckCouponActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        BottomNavigationView bnv = findViewById(R.id.bnv_adv1);
-        bnv.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // itme  : 내가 클릭한 항목에 대한 정보
-                // 정보 : 속성,id,title,icon...
-                // item.getItemId() : 항목의 id값을 가져오는 방법
-                int itemId = item.getItemId();
-                if (itemId == R.id.bnv_home) {
-                    intent = new Intent(CheckCouponActivity.this, UserActivity.class);
-                    startActivity(intent);
-                } else if (itemId == R.id.bnv_menu) {
-                    intent = new Intent(CheckCouponActivity.this, MenuRegiActivity.class);
-                    startActivity(intent);
-                } else if (itemId == R.id.bnv_addcoupon) {
-                    intent = new Intent(CheckCouponActivity.this, AddCouponActivity.class);
-                    startActivity(intent);
 
-                } else if (itemId == R.id.bnv_checkcoupon) {
-                    intent = new Intent(CheckCouponActivity.this, CheckCouponActivity.class);
-                    startActivity(intent);
-
-                }else if (itemId == R.id.bnv_info) {
-                    intent = new Intent(CheckCouponActivity.this, CheckCouponActivity.class);
-                    startActivity(intent);
-
-                }
-                // 항목에 대한 클릭 이벤트를 감지
-                // false : 클릭한번하고 이벤트가 계속 된다고 생각함
-                // true : 클릭 후 이벤트 종료
-                return true;
-            }
-        });
     }
 }
